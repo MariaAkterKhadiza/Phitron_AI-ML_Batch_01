c4c8a165d80606c00e92345082618e1fd2f00709
MariaAkterKhadiza <khadizatulmaria2002@gmail.com>
2025-10-05 02:35:08 +0800

    module1

--- python_basic(learning)/week_01/module_01/1.1.py
+++ python_basic(learning)/week_01/module_01/1.1.py
@@ -1,2 +1,9 @@
-x = 45
-print(x)
+# demo:core types & literals--
+x=43 #int
+y= 3.1417 #Float
+z="Maria" #string
+bool = True #bool
+List = [1,2,3,4,5] #(mutable)
+tpl = (1,2,3,4,4) #(immutable)
+dict = {"a":1,"b":2}#dict(mapping)
+st = {1,2,3,4} # set(unique)
